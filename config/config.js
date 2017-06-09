32db69b33be927018fa559d2a3427a235fd5b236
Esko Lehtonen <eplehton@turquino>
2017-06-09 18:28:52 +1200

    Release candidate

---
+++ config/config.js
@@ -0,0 +1,13 @@
+
+// SAGAME is a GLOBAL var defined in the freebike lib
+SAGAME.sagameStyle = 'selectone';
+SAGAME.feedback = 'description';
+SAGAME.showMarkers = 0;
+SAGAME.randomizeOrder = 1;
+SAGAME.clipIdLength = 11;
+SAGAME.calibInterval = 999;
+SAGAME.LOGURL = 'logdata.php';
+
+
+var clipsets_json = 'config/fl2016_clipsets_20170517.json';
+var targets_json = 'config/fl2016_targets_20170608.json';
