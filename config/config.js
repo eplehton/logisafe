55181f698eb6b5484d4764a438bdd65b0beefa2d
Esko Lehtonen <eplehton@turquino>
2017-08-23 18:41:01 +0200

    New clipsets and player point information.

--- config/config.js
+++ config/config.js
@@ -7,7 +7,10 @@ SAGAME.randomizeOrder = 1;
 SAGAME.clipIdLength = 11;
 SAGAME.calibInterval = 999;
 SAGAME.LOGURL = 'logdata.php';
+SAGAME.SAVESCORESURL = 'savescores.php';
 
+SAGAME.checkPlayerID = 1;
+SAGAME.validPlayerIDs = ['test', 'test1', 'test3']; // this is not security measure, but to limit using wrong ids
 
-var clipsets_json = 'config/fl2016_clipsets_20170517.json';
-var targets_json = 'config/fl2016_targets_20170609.json';
+var clipsets_json = 'config/fl2016_clipsets_20170819.json';
+var targets_json = 'config/fl2016_targets_20170614.json';
