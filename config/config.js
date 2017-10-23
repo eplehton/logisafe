3770c1530cea94246afe2e4b46ee39c0dedb770d
Esko Lehtonen <eplehton@turquino>
2017-10-23 08:17:01 +0200

    New layout for the start screen

--- config/config.js
+++ config/config.js
@@ -14,7 +14,18 @@ SAGAME.SAVESCORESURL = 'savescores.php';
 
 // this is not security measure, but to prevent using wrong ids
 SAGAME.checkPlayerID = 1;
-SAGAME.validPlayerIDs = ['test', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'ida1', 'ida2', 'esko1', 'esko2']; 
+SAGAME.validPlayerIDs = ['test', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'ida1', 'ida2', 'esko1', 'esko200']; 
 
 var clipsets_json = 'config/fl2016_clipsets_20170907.json';
 var targets_json = 'config/fl2016_targets_20171014.json';
+
+SAGAME.timeline = {'Test1' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'practice'}, 
+    'Test2' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Test1'}, 
+    'Test3' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Inter5'}, 
+    'Inter1' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Test2'}, 
+    'Inter2' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Inter1'}, 
+    'Inter3' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Inter2'}, 
+    'Inter4' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Inter3'}, 
+    'Inter5' : {'start' : '2017-09-01', 'end' :'2020-09-10', 'require': 'Inter4'} };
+    
+    
